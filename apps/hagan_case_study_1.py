1) Bug fix: fixed error in calculating sensitivy matrices. Algo now considerably faster. 2) Bug fix: constant derivative now returns array of proper shape. 3) Obtain functions in test_simple_two_layer_backprop from utils.

apps/hagan_case_study_1.py
@@ -1,10 +1,15 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import utils as nn_utils
 from nets.simple_two_layer_backprop import SimpleTwoLayerBackprop
 
-DATA_FILE_P = '../hagan_case_study_data/ball_p.txt'
-DATA_FILE_T = '../hagan_case_study_data/ball_t.txt'
+PYTHONPATH = os.environ.get('PYTHONPATH', None)
+if not PYTHONPATH:
+    raise ValueError('PYTHONPATH not set')
+
+DATA_FILE_P = '{}/hagan_case_study_data/ball_p.txt'.format(PYTHONPATH)
+DATA_FILE_T = '{}/hagan_case_study_data/ball_t.txt'.format(PYTHONPATH)
 
 
 def normalise_vector(vec):
@@ -151,7 +156,7 @@ def get_sse(W1, b1vec, W2, b2vec, V, y):
 kwargs = dict()
 kwargs['training_data'] = (train_input, train_target)
 kwargs['input_dim'] = train_input.shape[0]
-kwargs['layer1_neuron_count'] = 30
+kwargs['layer1_neuron_count'] = 3
 kwargs['layer2_neuron_count'] = 1
 kwargs['learning_rate'] = 0.01
 
@@ -163,7 +168,7 @@ kwargs['layer2_transfer_function_derivative'] = nn_utils.dpurelin
 # Instantiate backprop with init values
 sp = SimpleTwoLayerBackprop(** kwargs)
 
-iteration_count = 100000
+iteration_count = 10000
 logspace = np.logspace(1., np.log(iteration_count), 100)
 plot_points = [int(i) for i in list(logspace)]
 
@@ -197,3 +202,5 @@ for i in range(1, iteration_count):
         plt.scatter(train_target[0], sp.get_response(train_input), c='b')
         plt.scatter(0.5 + val_tar[0],  sp.get_response(val_inp), c='r')
         plt.draw()
+
+plt.savefig('hagan_case_study_1.png')

apps/hagan_sinus_fit_slow.py
@@ -50,7 +50,7 @@ kwargs['training_data'] = (train_input, train_target)
 kwargs['input_dim'] = train_input.shape[0]
 kwargs['layer1_neuron_count'] = 5
 kwargs['layer2_neuron_count'] = 1
-kwargs['learning_rate'] = 0.0001
+kwargs['learning_rate'] = 0.01
 
 kwargs['layer1_transfer_function'] = nn_utils.logsig
 kwargs['layer2_transfer_function'] = nn_utils.purelin
@@ -61,7 +61,7 @@ kwargs['layer2_transfer_function_derivative'] = nn_utils.dpurelin
 # Instantiate backprop with init values
 sp = SimpleTwoLayerBackprop(** kwargs)
 
-iteration_count = 10000000
+iteration_count = 100000
 logspace = np.logspace(1., np.log(iteration_count), 100)
 plot_points = [int(i) for i in list(logspace)]
 
@@ -72,6 +72,12 @@ plt.yscale('log')
 plt.xscale('log')
 plt.ion()
 plt.show()
+
+print('Initial weights:')
+sp.print_weights()
+print('Initial response:')
+print(sp.get_response(train_input))
+print('--')
 
 for i in range(1, iteration_count):
 

apps/test_simple_two_layer_backprop.py
@@ -1,5 +1,6 @@
 import numpy as np
 from nets.simple_two_layer_backprop import SimpleTwoLayerBackprop
+import utils as nn_utils
 
 
 def g(p):
@@ -23,34 +24,17 @@ def get_training_set_hagan_test_1(interpolate=False):
     return (x, g(x))
 
 
-def logsig(x):
-
-    return 1. / (1. + np.exp(-x))
-
-def dlogsig(x):
-    
-    e = np.exp(-x)
-    d = (1. + e)
-    return e / (d * d)
-
-def purelin(x):
-    return x
-
-def dpurelin(x):
-    return 1.
-
-
 kwargs = dict()
 kwargs['input_dim'] = 1
 kwargs['layer1_neuron_count'] = 2
 kwargs['layer2_neuron_count'] = 1
 kwargs['learning_rate'] = 0.1
 
-kwargs['layer1_transfer_function'] = logsig
-kwargs['layer2_transfer_function'] = purelin
+kwargs['layer1_transfer_function'] = nn_utils.logsig
+kwargs['layer2_transfer_function'] = nn_utils.purelin
 
-kwargs['layer1_transfer_function_derivative'] = dlogsig
-kwargs['layer2_transfer_function_derivative'] = dpurelin
+kwargs['layer1_transfer_function_derivative'] = nn_utils.dlogsig
+kwargs['layer2_transfer_function_derivative'] = nn_utils.dpurelin
 
 W1 = np.array([[-0.27], [-0.41]])
 b1vec = np.array([[-0.48], [-0.13]])
@@ -66,6 +50,9 @@ kwargs['training_data'] = (V, y)
 # Instantiate backprop with init values
 sp = SimpleTwoLayerBackprop(** kwargs)
 sp.train_step()
+
+print('\nWeights:')
+sp.print_weights()
 
 # Check values
 error_count = 0

nets/simple_two_layer_backprop.py
@@ -1,12 +1,8 @@
 import numpy as np
 
 
-def get_sensitivity_diag(S, df, a):
-
-    dig = np.zeros(S)
-    for i in range(S):
-        dig[i] = df(a[i])
-    return np.diag(dig)
+def get_sensitivity_diag(df, n):
+    return np.diag(np.transpose(df(n))[0])
 
 
 class SimpleTwoLayerBackprop():
@@ -74,10 +70,10 @@ class SimpleTwoLayerBackprop():
             n2vec = np.dot(self.W2, a1vec) + self.b2vec
             a2vec = self.f2(n2vec)
             
-            Fdot2 = get_sensitivity_diag(self.S2, self.df2, n2vec)
+            Fdot2 = get_sensitivity_diag(self.df2, n2vec)
             s2 = -2 * np.dot(Fdot2, tvec - a2vec)
 
-            Fdot1 = get_sensitivity_diag(self.S1, self.df1, n1vec)
+            Fdot1 = get_sensitivity_diag(self.df1, n1vec)
             s1 = np.dot(np.dot(Fdot1, np.transpose(self.W2)), s2)
 
             self.W2 = self.W2 - self.alpha  * np.dot(s2, np.transpose(a1vec))

utils.py
@@ -28,7 +28,7 @@ def purelin(x):
 
 
 def dpurelin(x):
-    return 1.
+    return np.ones(x.shape)
 
 
 def get_rms_error(dat_inp, dat_tar, sp):