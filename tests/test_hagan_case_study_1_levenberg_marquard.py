ea1f3e3c3b91406292328b67f1398f90ebe58620
Ytsen de Boer <yrdeboer@gmail.com>
2016-03-18 18:58:07 +0100

    Added also the Hagan case study as a test case and put all test cases in 1 directory.

--- tests/test_hagan_case_study_1_levenberg_marquard.py
+++ tests/test_hagan_case_study_1_levenberg_marquard.py
@@ -5,22 +5,21 @@ import utils as nn_utils
 from nets.levenberg_marquard_backprop import LevenbergMarquardBackprop
 from utils import print_dbg
 
-
-PYTHONPATH = os.environ.get('PYTHONPATH', None)
-if not PYTHONPATH:
-    raise ValueError('PYTHONPATH not set')
-
-DEBUG = False
+DEBUG = True
 
 DATA_FILE_P = 'hagan_case_study_data/ball_p.txt'
 DATA_FILE_T = 'hagan_case_study_data/ball_t.txt'
 
 def get_debug_weights():
 
-    W1 = np.array([[ 0.55324921,  0.50472091],[ 0.47403968,  0.52325356],[ 0.10596892,  0.18947488],[ 0.23209849,  0.76365438],[ 0.65728602,  0.28293854]])
-    b1 = np.array([[ 0.8749701] ,  [0.97889807],  [0.91427618],  [0.39205414],  [0.83271261]])
-    W2 = np.array([[ 0.26564146, 0.69353998, 0.39402367, 0.19349045, 0.31790691]])
-    b2 = np.array([[ 0.28713551]])
+    W1 = np.array([[-0.5324921, 0.472091],
+                   [0.47403968, -0.52325356],
+                   [-0.0596892, 0.28947488],
+                   [0.23209849, 0.86365438],
+                   [-0.728602, -0.93854]])
+    b1 = np.array([[ -0.8749701] ,  [0.889807],  [0.1427618],  [0.9205414],  [0.3271261]])
+    W2 = np.array([[ -0.26564146, 0.69353998, -0.39402367, 0.9349045, -0.1790691]])
+    b2 = np.array([[ -0.28713551]])
 
     return (W1, b1, W2, b2)
 
@@ -126,17 +125,44 @@ def plot_data():
     plt.scatter(y2[0], V2[1], c='r', marker='v')
 
     plt.show()
+
+
+def weights_as_expected(sp):
+
+    W1 = np.array([[-0.56356913, 0.54366279],
+                   [0.55272437, -0.72401577],
+                   [-0.26784775, 0.36279903],
+                   [0.45406544, 1.20365471],
+                   [-0.92410466, -0.86705573]])
+    b1vec = np.array([[-0.76825938],
+                      [0.60609654],
+                      [0.30372701],
+                      [0.63358324],
+                      [0.30658882]])
+    W2 = np.array([[-0.11766191,
+                    0.55124574,
+                    -0.5632504,
+                    0.76741378,
+                    -0.51184742]])
+    b2vec = np.array([[-0.78043624]])
+
+    mult = 1000000
+    a = np.array_equal(np.round(mult * sp.W1), np.round(mult * W1))
+    b = np.array_equal(np.round(mult * sp.b1vec), np.round(mult * b1vec))
+    c = np.array_equal(np.round(mult * sp.W2), np.round(mult * W2))
+    d = np.array_equal(np.round(mult * sp.b2vec), np.round(mult * b2vec))
     
+    return a & b & c & d
 
 (train_input, train_target, val_inp, val_tar) = get_data_sets()
 
-
-print('N_train = {} N_val = {}'.format(train_input.shape[1], val_inp.shape[1]))
+if not DEBUG:
+    print('N_train = {} N_val = {}'.format(train_input.shape[1], val_inp.shape[1]))
 
 kwargs = dict()
 kwargs['training_data'] = (train_input, train_target)
 kwargs['input_dim'] = train_input.shape[0]
-kwargs['layer1_neuron_count'] = 10
+kwargs['layer1_neuron_count'] = 5
 kwargs['layer2_neuron_count'] = 1
 
 kwargs['layer1_transfer_function'] = nn_utils.tansig
@@ -152,45 +178,63 @@ if DEBUG:
 # Instantiate backprop with init values
 sp = LevenbergMarquardBackprop(** kwargs)
 
-print_dbg('Weights:')
-sp.print_weights()
+if not DEBUG:
+    print_dbg('Weights:')
+    sp.print_weights()
 
 iteration_count = 1000
 logspace = np.logspace(1., np.log(iteration_count), 100)
 plot_points = [int(i) for i in list(logspace)]
 
-# Interactive plotting of the mean squared error
-plt.subplot(2,1,1)
-plt.axis([1, 10. * iteration_count, 1e-7, 10.])
-plt.yscale('log')
-plt.xscale('log')
-plt.ion()
+if not DEBUG:
+
+    # Interactive plotting of the mean squared error
+    plt.subplot(2,1,1)
+    plt.axis([1, 10. * iteration_count, 1e-7, 10.])
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.ion()
 
 for i in range(1, iteration_count):
 
     converged = sp.train_step()
 
-    if i in plot_points or converged:
+    if i < 25 or i in plot_points or converged:
 
-        rms_trn = sp.rms
-        rms_val = nn_utils.get_rms_error(val_inp, val_tar, sp)
+        if not DEBUG:
+        
+            rms_trn = sp.rms
+            rms_val = nn_utils.get_rms_error(val_inp, val_tar, sp)
+    
+            print('It={:6} rms_trn={:.5f}  rms_val={:.5f} '.format(
+                i, rms_trn, rms_val[0, 0]), end='')
+            print('g_norm={:.3f} conv={}'.format(sp.g_norm, converged))
 
-        print('It={:6} rms_trn={:.5f}  rms_val={:.5f} '.format(
-            i, rms_trn, rms_val[0, 0])),
-        print('g_norm={:.3f} conv={}'.format(sp.g_norm, converged))
+            plt.subplot(2, 1, 1)
+            plt.scatter(i, rms_trn, c='b')
+            plt.scatter(i, rms_val, c='r')
 
-        plt.subplot(2,1,1)
-        plt.scatter(i, rms_trn, c='b')
-        plt.scatter(i, rms_val, c='r')
+            plt.subplot(2, 1, 2)
+            plt.cla()
+            plt.scatter(train_target[0], sp.get_response(train_input), c='b')
+            plt.scatter(0.5 + val_tar[0],  sp.get_response(val_inp), c='r')
+            plt.show()
 
-        plt.subplot(2,1,2)
-        plt.cla()
-        plt.scatter(train_target[0], sp.get_response(train_input), c='b')
-        plt.scatter(0.5 + val_tar[0],  sp.get_response(val_inp), c='r')
-        plt.show()
-        plt.pause(.000001)  # Dont' remove this, needed for plotting, suck, yeah.
+            # Dont' remove this, needed
+            # for plotting, sucks, yeah.
+            plt.pause(.000001)  
 
         if converged:
             break
 
-plt.savefig('hagan_case_study_1_lb.png')
+if DEBUG:
+
+    test_name = 'Levenberg-Marquard Hagan Case study 1 -- light sensors'
+
+    if weights_as_expected(sp):
+        print('SUCCESS ({})'.format(test_name))
+    else:
+        print('ERROR   ({})'.format(test_name))
+
+else:
+    plt.savefig('hagan_case_study_1_lb.png')
